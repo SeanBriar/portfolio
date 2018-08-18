7526ba8c3f8beb2ca085f492b96ce9c1948d229d
Sean <seanoneill@Bons-MacBook-Pro.local>
2018-08-17 18:48:45 -0700

    scroll works!

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-// console.log('hi!');
+$(()=>{
+  $('.aboutMeLink').click(function(){
+       $('html, body').animate({
+           scrollTop: $('#aboutMe').position().top },
+           1000
+       );
+   });
+
+   $('.projectsLink').click(function(){
+        $('html, body').animate({
+            scrollTop: $('#projects').position().top },
+            1000
+        );
+    });
+
+     $('.contactLink').click(function(){
+          $('html, body').animate({
+              scrollTop: $('#contact').position().top },
+              1000
+          );
+      });
+})
